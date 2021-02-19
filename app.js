2aa7786ee01d15aca5f44d703b9068b968a6e74f
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2021-02-19 15:50:01 -0300

    Projeto pronto

---
+++ app.js
@@ -0,0 +1,22 @@
+function getDadosEnderecoPorCEP(cep){
+	console.log(cep)
+	let url = 'https://viacep.com.br/ws/' + cep + '/json/unicode/'
+	let xmlHttp = new XMLHttpRequest()
+	xmlHttp.open('GET', url)
+
+	xmlHttp.onreadystatechange = () => {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			let dadosJSONText = xmlHttp.responseText
+			let dadosJSONObj = JSON.parse(dadosJSONText) 
+
+			document.getElementById('rua').value = dadosJSONObj.logradouro
+			document.getElementById('bairro').value = dadosJSONObj.bairro
+			document.getElementById('cidade').value = dadosJSONObj.localidade
+			document.getElementById('uf').value = dadosJSONObj.uf
+
+			
+		}
+	}
+
+	xmlHttp.send()
+}
